4800a08b86fbbfc42a0e501ff7ece1ae25a8a8bc
Dustin Schimel <dustin.schimel@gmail.com>
2018-02-08 11:35:13 -0700

    Completed all assignment requirements

--- VectorPractice/Controller/Controller.cpp
+++ VectorPractice/Controller/Controller.cpp
@@ -19,5 +19,40 @@ Controller :: Controller()
 
 void Controller :: start()
 {
+    vector<int> intList(10);
+    vector<string> stringList(5);
     
+    for (int index = 0; index < 10; index++)
+    {
+        intList[index] = index;
+        cout << intList[index] << endl;
+    }
+    
+    cout << endl;
+    
+    for (int index = 0; index < 5; index++)
+    {
+        if (index == 0)
+        {
+            stringList[index] = "Zero";
+        }
+        if (index == 1)
+        {
+            stringList[index] = "One";
+        }
+        if (index == 2)
+        {
+            stringList[index] = "Two";
+        }
+        if (index == 3)
+        {
+            stringList[index] = "Three";
+        }
+        if (index == 4)
+        {
+            stringList[index] = "Four";
+        }
+        
+        cout << stringList[index] << endl;
+    }
 }
